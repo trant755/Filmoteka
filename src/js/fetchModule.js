f27f72b8b0e505950c2e35ec753057422e75f049
MakrFrost <frostmark@ukr.net>
2022-10-07 21:45:39 +0300

    Update fetchModule.js

--- src/js/fetchModule.js
+++ src/js/fetchModule.js
@@ -1,76 +1,79 @@
-//? Работающая версия
-
-const API_WEB = 'https://api.themoviedb.org/3/search/movie?';
+const API_WEB = 'https://api.themoviedb.org/3';
 const API_KEY = 'api_key=8a38092ae2d24fc685ebce0502669b7c';
 
-function fetchVideos(needFind) {
-  return fetch(
-    `https://api.themoviedb.org/3/movie/${needFind}?api_key=8a38092ae2d24fc685ebce0502669b7c&page=1&include_adult=true`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
+class MovieApiService {
+  constructor() {
+    this.needToFind = '';
+    this.page = 1;
+  }
+
+  fetchGenres() {
+    const responceGenres = fetch(
+      `${API_WEB}/genre/movie/list?${API_KEY}&language=en-US`
+    )
+      .then(responceGenres => {
+        if (!responceGenres.ok) {
+          throw new Error(responceGenres.status);
+        }
+        return response.json();
+      })
+      .catch(error => {
+        error;
+      });
+  }
+
+  fetchTrending() {
+    const responceTrending = fetch(
+      `${API_WEB}/trending/movie/day?${API_KEY}&page=${this.page}`
+    )
+      .then(responceTrending => {
+        if (!responceTrending.ok) {
+          throw new Error(responceTrending.status);
+        }
+        return response.json();
+      })
+      .catch(error => {
+        error;
+      });
+  }
+
+  fetchMovies() {
+    try {
+      const responce = fetch(
+        `${API_WEB}/search/movie?${API_KEY}&query=${this.needToFind}&page=${this.page}`
+      ).then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      });
+
+      const data = responce.data;
+
+      return data;
+    } catch (error) {
+      console.log('Error on try...catch', error);
     }
-    return response.json();
-  });
+  }
+
+  addMoviesPage(number) {
+    this.page = number;
+  }
+
+  resetMoviesPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.needToFind;
+  }
+
+  set query(newQuery) {
+    this.needToFind = newQuery;
+  }
 }
 
-fetchVideos('62');
-fetchVideos('100');
-fetchVideos('1');
-
-// ${ API_WEB }${ API_KEY }
-
-// https://api.themoviedb.org/3/movie/100?api_key=8a38092ae2d24fc685ebce0502669b7c
-
-//! Требует допилки
-//* import axios from "axios";
-
-// const API_WEB = "https://api.themoviedb.org/3/movie/";
-// const API_KEY = "?api_key=8a38092ae2d24fc685ebce0502669b7c&";
-// const API_OPTIONS = "page=1&include_adult=true";
-
-// class MovieApiService {
-//   constructor() {
-//     this.needToFind = "";
-//     this.page = 1;
-//   }
-
-//   fetchMovies() {
-//     try {
-//       const responce = fetch(
-//         `https://api.themoviedb.org/3/movie/100?api_key=8a38092ae2d24fc685ebce0502669b7c&page=1&include_adult=true`
-//       ).then((response) => {
-//         if (!response.ok) {
-//           throw new Error(response.status);
-//         }
-//         return response.json();
-//       });
-//       `${API_WEB}${API_KEY}q=${this.needToFind}${API_OPTIONS}&page=${this.page}`;
-//       const data = responce.data;
-
-//       this.addMovies();
-
-//       return data;
-//     } catch (error) {
-//       console.log("Error on try...catch", error);
-//     }
-//   }
-
-//   addMovies() {
-//     this.page += 1;
-//   }
-
-//   resetMovies() {
-//     this.page = 1;
-//   }
-
-//   get query() {
-//     return this.needToFind;
-//   }
-
-//   set query(newQuery) {
-//     this.needToFind = newQuery;
-//   }
-// }
-
-// console.log();
+const MovieApi = new MovieApiService();
+MovieApi.fetchMovies();
+MovieApi.fetchTrending();
+MovieApi.fetchGenres();
