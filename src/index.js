1f535d27e9998eb57dceb761fbec4b7a79db5b0e
Julia-Frost <juliafrostwork@gmail.com>
2022-10-08 12:49:42 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+<<<<<<< Updated upstream
 const Handlebars = require('handlebars');
 
+=======
+>>>>>>> Stashed changes
 import filmCardTpl from './templates/filmCards.hbs';
 
 console.log(filmCardTpl);
