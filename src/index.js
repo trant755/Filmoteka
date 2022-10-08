598245e933f34851cb4a02f6a25a7d52898c4b8d
MakrFrost <frostmark@ukr.net>
2022-10-08 23:59:01 +0300

    upd
    Добавляет кнопку перехода вверх с низа страницы

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './js/generateTrandingCard';
+import './js/buttonToTop';
--- src/js/buttonToTop.js
+++ src/js/buttonToTop.js
@@ -1,9 +1,21 @@
 const toTopBtn = document.querySelector('.button-to-top');
-console.log(toTopBtn);
-
 toTopBtn.addEventListener('click', onButtonClick);
 
 function onButtonClick() {
   window.scrollTo(0, 0);
-  console.log('sho?');
+}
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
+    toTopBtn.style.display = 'block';
+  } else {
+    toTopBtn.style.display = 'none';
+  }
 }
