b3315012bb30c3ed0b84391ef586ec43896dd915
Andrew Uzhva <103964378+trant755@users.noreply.github.com>
2022-10-09 16:06:32 +0300

    fix

--- src/js/onMarkupCards.js
+++ src/js/onMarkupCards.js
@@ -1,25 +1,30 @@
 import filmCards from '../templates/filmCards.hbs';
 
-export const onMarkupCards = function (films, container) {
+export const onMarkupCards = function (films, container, rating) {
   const markup = films
     .map(film => {
       const savedGenresId = localStorage.getItem('savedGenresId');
 
       const obje = {};
-      obje.release_date = film.release_date.slice(0, 4);
       obje.id = film.id;
-      obje.poster_path = '//image.tmdb.org/t/p/w400' + film.poster_path;
+      if (film.release_date.length > 0) {
+        obje.release_date = film.release_date.slice(0, 4);
+      }
+      if (film.poster_path.length > 0) {
+        obje.poster_path = '//image.tmdb.org/t/p/w400' + film.poster_path;
+      }
       obje.title = film.title;
-      // obje.vote_average = '';
-      obje.genres = film.genre_ids
-        .map(id => {
-          let genresArray = JSON.parse(savedGenresId).find(
-            obj => obj.id === id
-          );
+      if (rating) {
+        obje.vote_average = film.vote_average;
+      }
+      let genres = film.genre_ids.map(id => {
+        let genresArray = JSON.parse(savedGenresId).find(obj => obj.id === id);
 
-          return genresArray.name;
-        })
-        .join(', ');
+        return genresArray.name;
+      });
+      obje.genres =
+        genres.length > 3 ? genres.slice(0, 3).join(', ') : genres.join(', ');
+
       return filmCards(obje);
     })
     .join('');
