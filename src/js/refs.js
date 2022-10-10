fc79d4a5d8ba0a5b62dc090700432643d2870a83
MakrFrost <frostmark@ukr.net>
2022-10-10 22:59:02 +0300

    add

--- src/js/refs.js
+++ src/js/refs.js
@@ -3,4 +3,6 @@ export const refs = {
   trandingContainer: document.querySelector('#movie-container'),
   searchInput: document.querySelector('.header-search-form'),
   SearchErrMessage: document.querySelector('.header-message'),
+  watched: document.querySelector('.library--btn__watched'),
+  queue: document.querySelector('.library--btn__queue'),
 };
--- src/js/watchedQueue.js
+++ src/js/watchedQueue.js
@@ -1,15 +1,18 @@
-const watched = document.querySelector('.library--btn__watched');
-const queue = document.querySelector('.library--btn__queue');
+import localStorageApi from './local-storage-api/local-storage-api';
+const LS_API = new localStorageApi();
+import { refs } from './refs';
 
-watched.addEventListener('click', onClickWatched);
-queue.addEventListener('click', onClickQueue);
+refs.watched.addEventListener('click', onClickWatched);
+refs.queue.addEventListener('click', onClickQueue);
 
 function onClickWatched() {
-  watched.classList.add('library--btn--active');
-  queue.classList.remove('library--btn--active');
+  refs.watched.classList.add('library--btn--active');
+  refs.queue.classList.remove('library--btn--active');
+
+  console.log(LS_API.getFilmsFromWatched());
 }
 
 function onClickQueue() {
-  watched.classList.remove('library--btn--active');
-  queue.classList.add('library--btn--active');
+  refs.watched.classList.remove('library--btn--active');
+  refs.queue.classList.add('library--btn--active');
 }
