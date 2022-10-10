1e1ea63bde4c0615e5d8f9c1c5eac86b64301fa9
MakrFrost <frostmark@ukr.net>
2022-10-11 00:00:02 +0300

    Add remove class

--- src/js/watchedQueue.js
+++ src/js/watchedQueue.js
@@ -5,21 +5,36 @@ import { refs } from './refs';
 refs.watched.addEventListener('click', onClickWatched);
 refs.queue.addEventListener('click', onClickQueue);
 
+console.log(refs.movieContentBlock);
+
+let currentPage = '';
+
 onClickWatched();
 
 function onClickWatched() {
+  currentPage = 'watched';
   refs.watched.classList.add('library--btn--active');
   refs.queue.classList.remove('library--btn--active');
 
   if (LS_API.getFilmsFromWatched() && LS_API.getFilmsFromWatched.length > 0) {
     clearMoviesContainer();
-    refs.movieContentBlock.styles.display = 'none';
+    refs.movieContentBlock.classList.add('none');
+  } else {
+    refs.movieContentBlock.classList.remove('none');
   }
 }
 
 function onClickQueue() {
+  currentPage = 'queue';
   refs.watched.classList.remove('library--btn--active');
   refs.queue.classList.add('library--btn--active');
+
+  if (LS_API.getFilmsFromQueue && LS_API.getFilmsFromQueue.length > 0) {
+    clearMoviesContainer();
+    refs.movieContentBlock.classList.add('none');
+  } else {
+    refs.movieContentBlock.classList.remove('none');
+  }
 }
 
 function clearMoviesContainer() {
