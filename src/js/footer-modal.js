5de43352cac02c07c1b4e64d6a8f5237916ca24f
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-10-13 03:11:42 +0300

    111

--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -7,8 +7,32 @@
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  window.addEventListener("keydown", onPressKeyESC);
+
+    function onPressKeyESC(evt) {
+      if (evt.code === "Escape") {
+        toggleModal();
+      }
+    }
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+// ========================onPressKeyESC====================
+
+// const gallery = document.querySelector('[footer-modal]')
+
+// gallery.addEventListener('click', onGalleryClick);
+
+// function onGalleryClick(evt) {
+//    evt.preventDefault();
+
+//     window.addEventListener("keydown", onPressKeyESC, { once: true });
+//     function onPressKeyESC(evt) {
+//       if (evt.code === "Escape") {
+//         instanse.close();
+//       }
+//     }
+//   }
